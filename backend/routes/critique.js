3faa384c4d1b7114891a10e037b7c4853e3a8072
davemanroth <rothfarb@fas.harvard.edu>
2014-10-20 09:05:29 -0400

    Unresolved vis feedback storage problem. Switching branches to rebuild

--- backend/routes/critique.js
+++ backend/routes/critique.js
@@ -25,61 +25,47 @@ exports.feedbackSubmit = function (req, res) {
 				like: req.body.like,
 				improved: req.body.improved,
 			});
-			
-			/*
-			*/
+			if (details.length > 0 ) {
+				details.forEach( function (detail) {
+					photo1.details.push({
+						xCoord: detail.xCoord,
+						yCoord: detail.yCoord,
+						comment: detail.comment,
+					});
+				});
+			}
 			photo1.save(function (err, photo2) {
 				if (err) {
 					console.log(err);
 				}
 				else {
-					var newCrit = photo2.critiques.pop();
-				// add vis feedback data if there is any
-					if (details.length > 0 ) {
-						details.forEach( function (detail) {
-							/*
-								*/
-							photo2.details.push({
-								xCord: detail.xCoord,
-								yCord: detail.yCoord,
-								comment: detail.comment
-							});
-						});
-						photo2.save(function (err, photo3) {
-							if (err) {
-								console.log(err);
-							}
-							else {
-								res.json(photo3.critiques.pop());
-								//console.log(photo3);
-								/*
-								var newCritDets = photo3.critiques.pop();
-								for(var i = 0; i < details.length; i++) {
-									photo3.critiques._id(newCritDets._id).details.push(
-										mongoose.Type.ObjectId(photo3.details[details.length - i]._id));
-								}
-								photo3.save( function (err, photo4) {
-									if (err) {
-										console.log(err);
-									}
-									else {
-										res.json(newCritDets);
-										console.log(photo4);
-									}
-								});
-								*/
-							}
-						});
-					} //if details is not empty
-					else {
-						res.json(newCrit);
-						console.log(photo2);
+					res.json(photo2.critiques.pop());
+					User.addToArray('critiques', req.session.username, photo2._id);
+			/*
+			*/
+					var newCritDets = photo2.critiques.pop();
+					var detsSize = photo2.details.length - 1;
+					console.log(photo2.critiques.lastIndexOf());
+			/*
+					for(var i = 0; i < details.length; i++) {
+						photo2.critiques._id(newCritDets._id).details.push(
+							mongoose.Type.ObjectId(photo2.details[detsSize - i]._id));
 					}
+					photo2.save(function (err, photo3) {
+						if (err) {
+							console.log(err);
+						}
+						else {
+							res.json(photo3.critiques.pop());
+							User.addToArray('critiques', req.session.username, photo3._id);
+						}
+					});
+			*/
+				} //if details is not empty
+					
 				/*
 					*/
-					User.addToArray('critiques', req.session.username, photo1._id);
 					//console.log('Added critique to ' + req.session.username + '\'s account');
-				}
 			});
 		}
 	});
