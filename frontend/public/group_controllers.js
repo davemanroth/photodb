5b9a6fc79e58ffb37df3a6c2e904414725dc7273
davemanroth <rothfarb@fas.harvard.edu>
2014-12-13 17:38:06 -0500

    Reworked group listing to have Join group button show only when user is logged in

--- frontend/public/group_controllers.js
+++ frontend/public/group_controllers.js
@@ -30,38 +30,23 @@ angular.module('groupCtrl', [])
 				});
 
 			$scope.groupList.requestJoin = function (creator, sef_name) {
-				$http.get('/api/users/checkLoggedin')
-					.success( function (user) {
-						if (user === '0') {
-							messageCenterService.add(
-								'warning',
-								'You need to log in to join a group',
-								{ timeout : 3000 }
-							);
-						}
-						else {
-				/*
-							console.log(user);
-					*/
-							var joinInfo = {
-								creator : creator,
-								sef_group_name: sef_name,
-								requester : user.username
-							};
+				var joinInfo = {
+					creator : creator,
+					sef_group_name: sef_name,
+					requester : user.username
+				};
 
-							$http.post('/api/groups/request', joinInfo)
-								.success( function (data) {
-									messageCenterService.add(
-										'success',
-										'Your join request has been sent',
-										{ timeout : 3000 }
-									);
-								})
-								.error( function (err) {
-									console.log(err);
-								});
-						}
+				$http.post('/api/groups/request', joinInfo)
+					.success( function (data) {
+						messageCenterService.add(
+							'success',
+							'Your join request has been sent',
+							{ timeout : 3000 }
+						);
 					})
+					.error( function (err) {
+						console.log(err);
+					});
 			}
 		}
 	]);
